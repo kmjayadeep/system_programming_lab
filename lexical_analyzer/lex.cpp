3205ccbe2e2ac299781bb0e055922656a75ad9b4
Jayadeep KM <kmjayadeep@gmail.com>
2016-05-04 13:03:17 +0530

    lex without regex

--- lexical_analyzer/lex.cpp
+++ lexical_analyzer/lex.cpp
@@ -1,67 +1,141 @@
 #include<iostream>
+#include<sstream>
 #include<fstream>
-#include<regex>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
-fstream fin;
+vector<string> delims = { "{", "}", "[", "]", ",", ";", "." , "(", ")" };
+vector<string> operators = { "+", "-", "*", "/","<", ">", "!", ">=", "<=", "!=", "&", "|", "||","<<",">>","=","+=","-=","*=","/=","++","--"};
+vector<string> keywords = {
+	"int", "float", "double", "char", "void",
+	"while", "for", "do", "if", "return",
+	"using", "namespace",
+	"cout", "cin"
+};
 
-#define NEWLINE "[NEWLINE]"
-#define NUMBER "[NUMBER]"
-#define VARIABLE "[VARIABLE]"
-#define KEYWORD "[KEYWORD]"
-#define OPERATOR "[OPERATOR]"
-#define EQUAL "[EQUAL]"
+bool isIn(vector<string>& vec,string token){
+	return find(vec.begin(),vec.end(),token)!=vec.end();
+}
 
-regex var("[a-zA-Z][a-zA-Z0-9]*");
-regex num("[0-9]+");
-regex key("print");
-regex op("[\\+\\*\\-]");
-regex eq("=");
+bool isPreproc(string token){
+	return token[0]=='#'&&token.substr(1,8)=="include<"&&token.back()=='>';
+}
 
-bool analyze(string word){
-	if(regex_match(word,key))
-		cout<<KEYWORD;
-	else if(regex_match(word,var))
-		cout<<VARIABLE;
-	else if(regex_match(word,num))
-		cout<<NUMBER;
-	else if(regex_match(word,op))
-		cout<<OPERATOR;
-	else if(regex_match(word,eq))
-		cout<<EQUAL;
-	else{
-		cout<<endl<<"syntax error"<<endl;
+bool isDelim(string token){
+	return isIn(delims,token);
+}
+
+bool isKeyword(string token){
+	return isIn(keywords,token);
+}
+
+bool isOperator(string token){
+	return isIn(operators,token);
+}
+
+bool isIdentifier(string token){
+	if(!isalpha(token[0]))
 		return false;
-	}
+	for(int i=1;i<token.length()-1;i++)
+		if(!isalnum(token[i])&&token[0]!='_')
+			return false;
 	return true;
+}
 
+bool isNum(string token){
+	for(auto i=token.begin();i!=token.end();i++)
+		if(!isdigit(*i))
+			return false;
+	return true;
+}
+
+string check(string token,string tail){
+	if(isPreproc(token)){
+		return "Preprocessor";
+	}else if(isDelim(token)){
+		return "Delimiter";
+	}else if(isKeyword(token)){
+		return "Keyword";
+	}else if(tail!=""&&isOperator(token)&&isOperator(token+tail[0])){
+		return "";
+	}else if(isOperator(token)){
+		return "Operator";
+	}
+	return "";
+}
+
+string checkValid(string token){
+	if(token=="")
+		return "";
+	if(isIdentifier(token))
+		return "Identifier";
+	else if(isNum(token))
+		return "Number";
+	return "";
+}
+
+void analyze(string s){
+	string temp="";
+	for(auto i = s.begin();i!=s.end();i++){
+		temp+=*i;
+		auto j=i;
+		j++;
+		string ch = check(temp,string(j,s.end()));
+		if(ch!=""){
+			cout<<ch<<" :\t"<<temp<<endl;
+			temp="";
+		}else{
+			if(!isalnum(*i)){
+				string t = temp.substr(0,temp.length()-1);
+				ch = checkValid(t);
+				if(ch!=""){
+					cout<<ch<<" :\t"<<t<<endl;
+					temp = "";
+					i--;
+				}
+			}
+		}
+	}
+	if(temp!="")
+		cout<<"Invalid Token"<<endl;
+}
+
+bool isWhiteSpace(string s){
+	for(auto i=s.begin();i!=s.end();i++)
+		if(*i!=' '&&*i!='\t')
+			return false;
+	return true;
 }
 
 int main(int argc, char* argv[]){
 	if(argc<2){
-		cout<<"invalid input";
+		cout<<"invalid arguments"<<endl;
 		return 0;
 	}
-	fin.open(argv[1]);
+	ifstream fin(argv[1]);
 	string line;
-	string word;
-	int j,m;
+	int lineNo = 1;
 	while(getline(fin,line)){
-		cout<<line<<endl;
-		j=0;
-		while(j!=-1){
-			m = line.find(' ',j);
-			word = line.substr(j,m-j);
-			if(!analyze(word))
-				return 0;
-			// cout<<'\t';
-			if(m!=-1)
-				j=m+1;
-			else
-				j=-1;
+		if(isWhiteSpace(line)){
+			lineNo++;
+			continue;
 		}
-		cout<<NEWLINE<<endl;
+		cout<<endl<<"Line "<<lineNo++<<"\t : "<<line<<endl<<"-----------------"<<endl;
+		stringstream ss(line);
+		string el;
+		while(ss>>el){
+			analyze(el);
+		}
+
+
+		cout<<endl;
 	}
+
+
+
+
+
 	return 0;
 }
